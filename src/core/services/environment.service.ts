ca582a7c9b901b12576c2c59badb310db1769648
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-19 15:09:18 -0300

    fix: testing envs

--- src/core/services/environment.service.ts
+++ src/core/services/environment.service.ts
@@ -6,7 +6,7 @@ export class EnvironmentService {
     constructor() { }
 
     init(): Environment {
-        return {
+        const envs = {
             env: process.env.NODE_ENV || 'development',
             port: parseInt(process.env.PORT, 10) || 3000,
             token: process.env.TOKEN,
@@ -18,6 +18,9 @@ export class EnvironmentService {
                 db: process.env.PG_DB
             }
         }
+
+        console.log('processes', envs);
+        return envs;
     }
 }
 
