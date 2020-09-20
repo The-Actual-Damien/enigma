d85523823ab01f8f431e529dd43de923e5e8b632
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-20 13:38:42 -0300

    feat: allow protected envs to be inputed

--- src/core/services/environment.service.ts
+++ src/core/services/environment.service.ts
@@ -3,10 +3,7 @@ import { Environment } from '..'
 
 @Injectable()
 export class EnvironmentService {
-    private protectedEnvironments: Array<String>;
-    constructor() {
-        this.protectedEnvironments = ['staging', 'production'];
-    }
+    constructor(private protectedEnvironments: Array<String> = ['staging', 'production']) { }
 
     init(): Environment {
         const currentEnvironment = process.env.NODE_ENV || 'development';
@@ -26,17 +23,3 @@ export class EnvironmentService {
         }
     }
 }
-
-// export const loadConfig = (): Environment => ({
-//     env: process.env.NODE_ENV || 'development',
-//     port: parseInt(process.env.PORT, 10) || 3000,
-//     token: process.env.TOKEN,
-//     postgres: {
-//         host: process.env.PG_HOST,
-//         port: parseInt(process.env.PG_PORT, 10) || 5432,
-//         user: process.env.PG_USER,
-//         pass: process.env.PG_PASS,
-//         db: process.env.PG_DB
-//     }
-// })
-
--- src/core/services/swagger-config.service.ts
+++ src/core/services/swagger-config.service.ts
@@ -16,14 +16,3 @@ export class SwaggerConfigService {
         SwaggerModule.setup(config.route, app, docs);
     }
 }
-
-
-// export const addSwagger = (app: INestApplication, config: SwaggerConfig) => {
-//     const options = new DocumentBuilder()
-//         .setTitle(config.title)
-//         .setDescription(config.description)
-//         .setVersion(config.version)
-//         .build();
-//     const docs = SwaggerModule.createDocument(app, options);
-//     SwaggerModule.setup(config.route, app, docs);
-// }
